fd0e45ffffa6d03bddb6ec820007f43faa989580
ShawnArnold1 <shawnthearnold@gmail.com>
2019-01-07 16:47:01 -0600

    Updated java exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,54 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        String myString = "Make violence not war";
+        int myFavoriteNumber = 1337;
+        float myNumber;
+        System.out.println("Hello World");
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        myNumber = 3.14F;
+        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+
+//        int x = 4;
+//        x += 5;
+
+
+//        int x = 3;
+//        int y = 4;
+//        x+=20
+//        y*=x;
+
+//        System.out.println(y);
+
+
+        int x = 10;
+        int y = 2;
+        x/=y;
+        y -=x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        double pi = 3.14159;
+
+
+        System.out.println("The Value of Pie is approximately " + pi);
+    }
+}
+
